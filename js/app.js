c9db1e152781484c00678653fe7ce939d59c26bb
FrootyRoman <302445159@student.rocmondriaan.nl>
2023-06-18 20:58:50 +0200

    about me page afgemaakt
    ik heb mijn about me page afgemaakt en met behulp van grid mijn skills laten zien

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const scene = document.getElementById('scene');
 const parallaxInstance = new Parallax(scene);
 
 msg = "🧛";
-msg = "  Never Trust No Bih" + msg;position = 0;
+msg = "  Portfolio Roman " + msg;position = 0;
 function scrolltitle() {
 document.title = msg.substring(position, msg.length) + msg.substring(0, position); position++;
 if (position > msg.length) position = 0
